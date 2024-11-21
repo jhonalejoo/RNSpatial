1669a7a0f9ca625a6a7ef217d370acf71efbec55
jhonalejoo <42988740+jhonalejoo@users.noreply.github.com>
2024-11-21 13:30:13 -0500

    old arch

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,20 @@
-const FncSpatialite = require('./NativeRNSpatial').default;
+import { NativeModules, Platform } from 'react-native';
 
-export default FncSpatialite;
+const LINKING_ERROR =
+  `The package 'react-native-fnc-spatialite' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const RNSpatial = NativeModules.RNSpatial
+  ? NativeModules.RNSpatial
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export default RNSpatial;
